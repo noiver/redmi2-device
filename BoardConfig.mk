1d1ee0928feb90c56e4d29a8071f8c53f873c49c
noiver <1017227274@qq.com>
2016-10-20 23:10:24 +0800

    sy

---
+++ BoardConfig.mk
@@ -0,0 +1,98 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+FORCE_32_BIT := true
+
+include device/wingtech/msm8916-common/BoardConfigCommon.mk
+DEVICE_PATH := device/wingtech/wt88047
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME :=
+
+# Kernel
+BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
+BOARD_KERNEL_CMDLINE += phy-msm-usb.floated_charger_enable=1 androidboot.selinux=permissive
+TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
+TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
+
+# CPU
+TARGET_CPU_CORTEX_A53 := true
+
+# WT88047 init
+TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_wt88047.cpp
+TARGET_UNIFIED_DEVICE := true
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Display
+TARGET_CONTINUOUS_SPLASH_ENABLED := false
+
+# Charger
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.wt88047
+
+# Camera
+BOARD_CAMERA_SENSORS := \
+    ov2680_5987fhq \
+    ov8865_q8v18a \
+    ov2680_skuhf
+TARGET_USE_VENDOR_CAMERA_EXT := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+
+# DPM NSRM Feature
+TARGET_LDPRELOAD := libNimsWrap.so
+
+# Flags
+# COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD -DCONFIG_WT88047_CAMERA -DCAMERA_VENDOR_L_COMPAT
+
+# GPS
+USE_DEVICE_SPECIFIC_GPS := true
+TARGET_NO_RPC := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 335544320
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5939100672
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+ifeq ($(RECOVERY_VARIANT),twrp)
+DEVICE_RESOLUTION := 720x1280
+TW_TARGET_USES_QCOM_BSP := false
+TW_NEW_ION_HEAP := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_INCLUDE_CRYPTO := true
+RECOVERY_SDCARD_ON_DATA := true
+endif
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+ANDROID_JACK_VM_ARGS := -Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096M
+# inherit from the proprietary version
+-include vendor/wingtech/wt88047/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,16 @@
+Device configuration for XIAOMI Redmi 2 (wt88047).
+==================================================
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Quad-core 1.2 GHz Cortex-A53
+CHIPSET | Qualcomm MSM8916 Snapdragon 410
+GPU     | Adreno 306
+Memory  | 1GB/2GB RAM.
+Shipped Android Version | 4.4.4
+Storage | 8GB/16GB
+Battery | 2200 mAh
+Display | 720 x 1280 pixels (~312 ppi pixel density), 4.7 inches (~67.8% screen-to-body ratio)
+Rear Camera  | 8 MP, f/2.2, 28mm, autofocus, LED flash
+Front Camera | 2 MP, 720p
+Release Date | January/August 2015
